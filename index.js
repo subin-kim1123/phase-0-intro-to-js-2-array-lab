91e9c4a1ad37a6348b5b0e288cab7b04e978a915
Subin Kim <https://github.com/subin-kim1123>
2021-04-18 17:01:45 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+    return destructivelyAppendCat;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+    return destructivelyPrependCat;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+    return destructivelyRemoveLastCat;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+    return destructivelyRemoveFirstCat;
+}
+
+
+const appendCat = cats.slice();
+
+appendCat.push('Broom');
+
